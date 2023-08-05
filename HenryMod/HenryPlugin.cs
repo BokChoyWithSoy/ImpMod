df0dbc6ab460ec04470c5037935f5af39165b66f
BokChoyWithSoy <42159993+BokChoyWithSoy@users.noreply.github.com>
2023-08-06 00:29:09 +1000

    First commit

---
+++ HenryMod/HenryPlugin.cs
@@ -0,0 +1,103 @@
+﻿using BepInEx;
+using BepInEx.Bootstrap;
+using HenryMod.Modules.Survivors;
+using R2API.Utils;
+using RoR2;
+using System.Collections.Generic;
+using System.Security;
+using System.Security.Permissions;
+using UnityEngine;
+using EmotesAPI;
+
+[module: UnverifiableCode]
+[assembly: SecurityPermission(SecurityAction.RequestMinimum, SkipVerification = true)]
+
+//rename this namespace
+namespace HenryMod
+{
+    [BepInDependency("com.bepis.r2api", BepInDependency.DependencyFlags.HardDependency)]
+    [BepInDependency("com.weliveinasociety.CustomEmotesAPI", BepInDependency.DependencyFlags.SoftDependency)]
+    [NetworkCompatibility(CompatibilityLevel.EveryoneMustHaveMod, VersionStrictness.EveryoneNeedSameModVersion)]
+    [BepInPlugin(MODUID, MODNAME, MODVERSION)]
+    [R2APISubmoduleDependency(new string[]
+    {
+        "PrefabAPI",
+        "LanguageAPI",
+        "SoundAPI",
+        "UnlockableAPI"
+    })]
+
+    public class HenryPlugin : BaseUnityPlugin
+    {
+        // if you don't change these you're giving permission to deprecate the mod-
+        //  please change the names to your own stuff, thanks
+        //   this shouldn't even have to be said
+        public const string MODUID = "com.Bok.KiryuMod";
+        public const string MODNAME = "KiryuMod";
+        public const string MODVERSION = "1.0.0";
+
+        // a prefix for name tokens to prevent conflicts- please capitalize all name tokens for convention
+        public const string DEVELOPER_PREFIX = "BOK";
+
+        public static HenryPlugin instance;
+
+        private void Awake()
+        {
+            instance = this;
+
+            Log.Init(Logger);
+            Modules.Assets.Initialize(); // load assets and read config
+            Modules.Config.ReadConfig();
+            Modules.States.RegisterStates(); // register states for networking
+            Modules.Buffs.RegisterBuffs(); // add and register custom buffs/debuffs
+            Modules.Projectiles.RegisterProjectiles(); // add and register custom projectiles
+            Modules.Tokens.AddTokens(); // register name tokens
+            Modules.ItemDisplays.PopulateDisplays(); // collect item display prefabs for use in our display rules
+
+            // survivor initialization
+            new MyCharacter().Initialize();
+
+            // now make a content pack and add it- this part will change with the next update
+            new Modules.ContentPacks().Initialize();
+
+            Hook();
+        }
+
+        private void Hook()
+        {
+            if (Chainloader.PluginInfos.ContainsKey("com.weliveinasociety.CustomEmotesAPI"))
+            {
+                On.RoR2.SurvivorCatalog.Init += SurvivorCatalog_Init;
+            }
+
+            // run hooks here, disabling one is as simple as commenting out the line
+            On.RoR2.CharacterBody.RecalculateStats += CharacterBody_RecalculateStats;
+        }
+
+        private void CharacterBody_RecalculateStats(On.RoR2.CharacterBody.orig_RecalculateStats orig, CharacterBody self)
+        {
+            orig(self);
+
+            // a simple stat hook, adds armor after stats are recalculated
+            if (self)
+            {
+                if (self.HasBuff(Modules.Buffs.armorBuff))
+                {
+                    self.armor += 300f;
+                }
+            }
+        }
+
+        private void SurvivorCatalog_Init(On.RoR2.SurvivorCatalog.orig_Init orig)
+        {
+            orig();
+            foreach (var item in SurvivorCatalog.allSurvivorDefs)
+            {
+                if (item.bodyPrefab.name == "KiryuBody")
+                {
+                    CustomEmotesAPI.ImportArmature(item.bodyPrefab, Modules.Assets.mainAssetBundle.LoadAsset<GameObject>("KiryuHumanoid"));
+                }
+            }
+        }
+    }
+}
